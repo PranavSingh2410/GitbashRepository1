dc3a36081f04553eb9d02dde028294b883004f94
PranavSingh2410 <pranavks223@gmail.com>
2023-12-06 08:37:05 +0530

    commit1

---
+++ src/test/java/Project/Project1.java
@@ -0,0 +1,14 @@
+package Project;
+
+import org.testng.annotations.Test;
+
+public class Project1 {
+
+	
+	@Test
+	public void M1() 
+	{
+		System.out.println("new class for branch");
+	}
+
+}
---
+++ src/test/java/Project/Project2.java
@@ -0,0 +1,13 @@
+package Project;
+
+import org.testng.annotations.Test;
+
+public class Project2 {
+
+	@Test
+	public void M2()
+	{
+		System.out.println(" new project for git conflict and branch");
+	}
+
+}
